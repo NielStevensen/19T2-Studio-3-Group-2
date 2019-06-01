09214692cb2010687d3a5e20bc54a232759495db
Nielstevensen <Stevennielsen4@gmail.com>
2019-06-01 15:35:03 +1000

    added tile swaping using rgular mouse controlls / refactored script
    added regular click controlls and reformated scripts

--- Assets/Scripts/PopulateTiles.cs
+++ Assets/Scripts/PopulateTiles.cs
@@ -14,10 +14,12 @@ public class PopulateTiles : MonoBehaviour
     TileStates Temp; // useed to hold data while transfered
     float lastMouseX;
     public Camera cam;
+    public int[] selected;
 
     // Start is called before the first frame update
     void Start()
     {
+        selected = new int[2];
         tileArray = new TileStates[gridX, gridY];
         savedTiles  = new GameObject[gridX, gridY];
         for (float x = 0; x <gridX; x++)
@@ -27,7 +29,7 @@ public class PopulateTiles : MonoBehaviour
                 // nightmare equation for equal distribution on 8*8 grid
                 Renderer Renderer = GetComponent<Renderer>(); // makes next line shorter and easdier to understand
                 placementPos = new Vector3(transform.position.x - (Renderer.bounds.size.x/2) +(Renderer.bounds.size.x *(((2 * x) + 1) / (gridX *2))), transform.position.y - (Renderer.bounds.size.y / 2) + (Renderer.bounds.size.y * (((2 * y) + 1) / (gridY*2))));
-                tileArray[Mathf.FloorToInt(x), Mathf.FloorToInt(y)] = (TileStates)Random.Range(0, 6); // spawn with random values and save to arrayof tile types
+                tileArray[Mathf.FloorToInt(x), Mathf.FloorToInt(y)] = (TileStates)Random.Range(0, 6); // spawn with random values and save to array of tile types
                 savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)] = Instantiate(tile, placementPos, Quaternion.identity,transform); // create a ytile and save to arry of tiles as ojects
                 // asign colours update to change spties instead once avaliable
                 AssignColor(Mathf.FloorToInt(x), Mathf.FloorToInt(y));
@@ -56,42 +58,45 @@ public class PopulateTiles : MonoBehaviour
             }
         }
         //movement
-        if (Input.GetButtonDown("Fire1"))
-        { 
-            Vector3 selectionPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-            for (float x = 1; x < gridX; x++)
+        Vector3 selectionPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) // set position on firsat frame button is held
+        {
+            for (int x = 1; x < gridX; x++)
             {
-                for (float y = 0; y < gridY; y++)
+                for (int y = 0; y < gridY; y++)
                 {
-                    // check bounds
-                    if(selectionPoint.x > savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].transform.position.x - savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].GetComponent<SpriteRenderer>().bounds.extents.x)
+                    if (CheckBounds(x, y, selectionPoint))
                     {
-                        if (selectionPoint.x < savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].transform.position.x + savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].GetComponent<SpriteRenderer>().bounds.extents.x)
-                        {
-                            if (selectionPoint.y < savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].transform.position.y + savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].GetComponent<SpriteRenderer>().bounds.extents.y)
-                            {
-                                if (selectionPoint.y > savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].transform.position.y - savedTiles[Mathf.FloorToInt(x), Mathf.FloorToInt(y)].GetComponent<SpriteRenderer>().bounds.extents.y)
-                                {
-                                    Temp = tileArray[Mathf.FloorToInt(x - 1), Mathf.FloorToInt(y)];
-                                    tileArray[Mathf.FloorToInt(x - 1), Mathf.FloorToInt(y)] = tileArray[Mathf.FloorToInt(x), Mathf.FloorToInt(y)];
-                                    tileArray[Mathf.FloorToInt(x), Mathf.FloorToInt(y)] = Temp;
-                                }
-                            }
-                        }          
+                        lastMouseX = Input.mousePosition.x;
+                        selected[0] = x;
+                        selected[1] = y;
                     }
-                } 
+                }
             }
         }
-        else
+        else if (Input.GetMouseButton(0)) //check each consescutive frame
         {
-
-        }
-        //redraw grid
-        for (float x = 0; x < gridX; x++)
-        {
-            for (float y = 0; y < gridY; y++)
+            for (int x = 0; x < gridX; x++)
             {
-                AssignColor(Mathf.FloorToInt(x), Mathf.FloorToInt(y));
+                for (int y = 0; y < gridY; y++)
+                {
+                    if (CheckBounds(x, y, selectionPoint))
+                    {
+                        if (x != selected[0])
+                        {
+                            MouseSwap(x, y);
+                        }
+                    }
+                }
+            }
+        }
+
+        //redraw grid
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int y = 0; y < gridY; y++)
+            {
+                AssignColor(x, y);
             }
         }
     }
@@ -132,4 +137,51 @@ public class PopulateTiles : MonoBehaviour
                 }
         }
     } // update colours
+
+    void CursorSwap(int x, int y)
+    {
+        Temp = tileArray[x,y];
+        tileArray[x,y] = tileArray[x,y];
+        tileArray[x,y] = Temp;
+    }
+
+    void MouseSwap(int x, int y)
+    {
+        if (Input.mousePosition.x > lastMouseX)
+        {
+            Temp = tileArray[x, y];
+            tileArray[x, y] = tileArray[x - 1, y];
+            tileArray[x - 1, y] = Temp;
+
+        }
+        if (Input.mousePosition.x < lastMouseX)
+        {
+            Temp = tileArray[x, y];
+            tileArray[x, y] = tileArray[x + 1, y];
+            tileArray[x + 1, y] = Temp;
+
+        }
+        lastMouseX = Input.mousePosition.x;
+        selected[0] = x;
+
+    }
+
+    private bool CheckBounds(int x, int y, Vector3 selectionPoint)
+    {
+        bool flag = false;
+        if (selectionPoint.x > savedTiles[x, y].transform.position.x - savedTiles[x, y].GetComponent<SpriteRenderer>().bounds.extents.x)
+        {
+            if (selectionPoint.x < savedTiles[x, y].transform.position.x + savedTiles[x, y].GetComponent<SpriteRenderer>().bounds.extents.x)
+            {
+                if (selectionPoint.y < savedTiles[x, y].transform.position.y + savedTiles[x, y].GetComponent<SpriteRenderer>().bounds.extents.y)
+                {
+                    if (selectionPoint.y > savedTiles[x, y].transform.position.y - savedTiles[x, y].GetComponent<SpriteRenderer>().bounds.extents.y)
+                    {
+                        flag = true;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
 }
