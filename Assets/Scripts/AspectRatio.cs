a3b040d95cbe465c54dd1bab85af1994d70e60ae
a-clements <antony.clements@bigpond.com>
2019-06-03 09:14:34 +1000

    copying assets from Stephens branch to the alternate branch

---
+++ Assets/Scripts/AspectRatio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatio : MonoBehaviour
+{
+    private float Width;
+    private float Height;
+
+    public Camera MainCamera;
+
+
+    void Start()
+    {
+        Width = MainCamera.orthographicSize * MainCamera.aspect;
+        Height = MainCamera.orthographicSize;
+    }
+
+    void Update()
+    {
+        MainCamera.orthographicSize = Width / MainCamera.aspect;
+
+        MainCamera.transform.position = new Vector3(1 * (Width - MainCamera.orthographicSize * MainCamera.aspect), 1 * (Height - MainCamera.orthographicSize), MainCamera.transform.position.z);
+    }
+}
