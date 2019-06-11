a3954202d5d882b0c046972b1c3bdfede880190c
NielStevensen <40745561+NielStevensen@users.noreply.github.com>
2019-06-11 17:02:26 +1000

    Charge attack WIP
    added basics of charge attack WIP had to move rooms

---
+++ Assets/Scripts/ChargeAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeAttack : MonoBehaviour
+{
+
+    public Image Bar;
+
+    [Tooltip("total capacity of bar")]
+    public float capacity;
+
+    [Tooltip("current capacity of bar")]
+    public float Current;
+
+    public float aCount;
+    public float bCount;
+    public float cCount;
+    public float dCount;
+    public float eCount;
+
+    void FillBar(BlockTypes colour,int chainCount, int comboCount)
+    {
+            switch (colour)
+            {
+                case BlockTypes.A:
+                aCount += comboCount;
+
+                    break;
+                case BlockTypes.B:
+                    bCount += comboCount;
+
+                    break;
+                case BlockTypes.C:
+                    cCount += comboCount;
+
+                    break;
+                case BlockTypes.D:
+                    dCount += comboCount;
+
+                    break;
+                case BlockTypes.E:
+                    eCount += comboCount;
+
+                    break;
+            }
+        
+    }
+}
