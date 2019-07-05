b2e07a7f2b54dd315e29521e0b481409ffb25bce
Gords1229 <40745592+Gords1229@users.noreply.github.com>
2019-07-06 01:30:43 +1000

    camera control script added to my test scene

---
+++ Assets/Scripts/CameraTesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTesting : MonoBehaviour
+{
+    public Camera cam;
+    public float defaultCamSize;
+    public float newCamSize;
+    public float horizontalCamSize;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calculating the default horizontal cam size determinced by the orthgraphic size
+        //0.5625 is the aspect ratio of 9:16, 
+        //using this ratio as we are setting that as our standard when designing the layouts
+        horizontalCamSize = defaultCamSize * 0.5625f;
+
+        //calculating and setting the new orthgraphic Size
+        newCamSize = horizontalCamSize / cam.aspect;
+        cam.orthographicSize = newCamSize;
+
+        //moving the camera so the botton of the screen also in place.
+        float newCamPositionY = cam.transform.position.y - (defaultCamSize - newCamSize);
+        cam.transform.position = new Vector3(cam.transform.position.x, newCamPositionY, cam.transform.position.z);
+    }
+}
