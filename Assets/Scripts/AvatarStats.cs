d19a720c9da97643e0658b99a6b346786b88ec00
NielStevensen <40745561+NielStevensen@users.noreply.github.com>
2019-07-05 16:03:11 +1000

    stat block for avatar

---
+++ Assets/Scripts/AvatarStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarStats : MonoBehaviour
+{
+
+}
+
+
+[System.Serializable]
+public class StatBlock
+{
+    public int element;
+    public float maxHealth;
+    public float attack;
+    public float defence;
+    public string ability;
+
+}
