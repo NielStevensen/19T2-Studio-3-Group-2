2443849c8272059e5d21a66ac3029fb4275b0a79
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-07-27 14:30:45 +1000

    made some adjustments for the new camera layout
    ProfilePanel, SettingsPanel, ShopPanel and the AspectRatio script has new values.

--- Assets/Scripts/AspectRatio.cs
+++ Assets/Scripts/AspectRatio.cs
@@ -108,24 +108,25 @@ public class AspectRatio : MonoBehaviour
             #endregion
 
             #region Shop Menu
-                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().padding.bottom = 300;
-                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().cellSize = new Vector2(240, 240);
-                ShopMenu.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector4(-125f, -246f, 125f, 246f);
-                ShopMenu.transform.GetChild(1).GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector4(0f, 0f, 0f, 0f);
-                ShopMenu.transform.GetChild(1).GetChild(0).GetChild(2).GetComponent<RectTransform>().anchoredPosition = new Vector4(125f, -246f, -125f, 246f);
+                ShopMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.bottom = 300;
+                ShopMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().cellSize = new Vector2(240, 240);
+                ShopMenu.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector4(-125f, -246f, 125f, 246f);
+                ShopMenu.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector4(0f, 0f, 0f, 0f);
+                ShopMenu.transform.GetChild(0).GetChild(0).GetChild(2).GetComponent<RectTransform>().anchoredPosition = new Vector4(125f, -246f, -125f, 246f);
+
+                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().padding.top = 550;
+                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
 
                 ShopMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().padding.top = 550;
+                ShopMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
 
-                ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.top = 550;
+                ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.top = 750;
+                ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.right = 275;
                 ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
 
                 ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.top = 750;
-                ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.right = 275;
+                ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.left = 275;
                 ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
-
-                ShopMenu.transform.GetChild(5).GetComponent<GridLayoutGroup>().padding.top = 750;
-                ShopMenu.transform.GetChild(5).GetComponent<GridLayoutGroup>().padding.left = 275;
-                ShopMenu.transform.GetChild(5).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
             #endregion
 
             MainCamera.aspect = Aspectratio;
@@ -137,25 +138,25 @@ public class AspectRatio : MonoBehaviour
             Screen.orientation = ScreenOrientation.Landscape;
 
             #region Main Menu
-                MainMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.top = 300;
-                MainMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().cellSize = new Vector2(100, 25);
+                MainMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.top = 220;
+                MainMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().cellSize = new Vector2(150, 50);
                 MainMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().spacing = new Vector2(0, 25);
             #endregion
 
             #region Settings Menu
-                SettingsMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.left = 950;
+                SettingsMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.left = 975;
                 SettingsMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.bottom = 350;
-                SettingsMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().cellSize = new Vector2(300,65);
+                SettingsMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().cellSize = new Vector2(230,55);
 
-                SettingsMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().padding.top = 425;
+                SettingsMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().padding.top = 375;
                 SettingsMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
 
-                SettingsMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().padding.right = 725;
+                SettingsMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().padding.right = 785;
                 SettingsMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().padding.bottom = 350;
                 SettingsMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75,75);
 
-                SettingsMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.top = 25;
-                SettingsMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().cellSize = new Vector2(200,45);
+                SettingsMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.bottom = 30;
+                SettingsMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().cellSize = new Vector2(500,45);
                 SettingsMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().spacing = new Vector2(0, 25);
             #endregion
 
@@ -189,26 +190,26 @@ public class AspectRatio : MonoBehaviour
             #endregion
 
             #region Shop Menu
-                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().padding.bottom = 100;
-                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().cellSize = new Vector2(285, 285);
-                ShopMenu.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector4(-285f, 0f, 285f, 0f);
-                ShopMenu.transform.GetChild(1).GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector4(0f, 0f, 0f, 0f);
-                ShopMenu.transform.GetChild(1).GetChild(0).GetChild(2).GetComponent<RectTransform>().anchoredPosition = new Vector4(285f, 0f, -285f, 0f);
+                ShopMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().padding.bottom = 150;
+                ShopMenu.transform.GetChild(0).GetComponent<GridLayoutGroup>().cellSize = new Vector2(285, 285);
+                ShopMenu.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector4(-285f, 0f, 285f, 0f);
+                ShopMenu.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector4(0f, 0f, 0f, 0f);
+                ShopMenu.transform.GetChild(0).GetChild(0).GetChild(2).GetComponent<RectTransform>().anchoredPosition = new Vector4(285f, 0f, -285f, 0f);
 
-                ShopMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().padding.top = 270;
-                ShopMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().spacing = new Vector2(200, 0);
+                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().padding.top = 215;
+                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().spacing = new Vector2(200, 0);
+                ShopMenu.transform.GetChild(1).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
+
+                ShopMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().padding.top = 215;
                 ShopMenu.transform.GetChild(2).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
 
-                ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.top = 270;
+                ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.top = 375;
+                ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().padding.right = 725;
                 ShopMenu.transform.GetChild(3).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
 
-                ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.top = 425;
-                ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.right = 725;
+                ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.top = 375;
+                ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().padding.left = 725;
                 ShopMenu.transform.GetChild(4).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
-
-                ShopMenu.transform.GetChild(5).GetComponent<GridLayoutGroup>().padding.top = 425;
-                ShopMenu.transform.GetChild(5).GetComponent<GridLayoutGroup>().padding.left = 725;
-                ShopMenu.transform.GetChild(5).GetComponent<GridLayoutGroup>().cellSize = new Vector2(75, 75);
             #endregion
 
             MainCamera.aspect = (Width / Height) * (Screen.width / Screen.height);
