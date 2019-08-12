6f39d9be5773512be7fc0ba241d5219e8f388c72
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-08-13 08:36:44 +1000

    first round of fixes
    There is a lot that has been removed in the merge.

--- Assets/Scripts/GreyScale.cs
+++ Assets/Scripts/GreyScale.cs
@@ -8,15 +8,10 @@ public class GreyScale : MonoBehaviour
     ColorGrading ColourGrading = null; //ColorGrading is an effect of the post processand the variable needs to be initialised to null.
     PostProcessVolume PostProcess; //This is a post processing object that is filled with the post process component on the game object.
 
-    public bool Greyscale = false; //This is the bool that turns the post process on or off.
-
-    [Range(-100, 100)]
     public float Saturation; //This is the float variable that gives controls the saturation value of the post process stack. It has a range of -100 to 100.
 
-    [Range(-10, 10)]
     public float Contrast; //This is the float variable that gives controls the contrast value of the post process stack. It has a range of -10 to 10.
 
-    [Range(-5, 5)]
     public float Exposure; //This is the float variable that gives controls the contrast value of the post process stack. It has a range of -5 to 5.
 
     void Start ()
@@ -29,16 +24,9 @@ public class GreyScale : MonoBehaviour
     {
         /*If the GreyScale bool is true then the ColorGrading effect is turned on. While it is turned on any changes to the Saturation, Contrast, or Exposure slider on the gameobject will be directly reflected in the post process stack.*/
         /*If the GreyScale bool is false the ColorGrading effect is turned off and the screen returns to normal.                                                                                                                            */
-		if(Greyscale == true)
-        {
-            ColourGrading.enabled.value = true;
-            ColourGrading.saturation.value = Saturation;
-            ColourGrading.contrast.value = Contrast;
-            ColourGrading.postExposure.value = Exposure;
-        }
-        else
-        {
-            ColourGrading.enabled.value = false;
-        }
+
+        ColourGrading.saturation.value = Saturation;
+        ColourGrading.contrast.value = Contrast;
+        ColourGrading.postExposure.value = Exposure;
 	}
 }
