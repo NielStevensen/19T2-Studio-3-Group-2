bb836e205e705d50b76e5828d8a78b80e8d9b9f6
nviengkhou <40777998+nviengkhou@users.noreply.github.com>
2019-08-13 17:17:31 +1000

    Fixed back out button + imported recoloured sprites

---
+++ Assets/Scripts/BackOut.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackOut : MonoBehaviour
+{
+	[Tooltip("Name of the menu scene.")]
+	public string menu;
+
+	//Return to the menu
+	public void OnBackOut()
+	{
+		SceneManager.LoadScene(menu);
+	}
+}
