efeed84942c49c881382986b7504d1c137339ddf
NielStevensen <40745561+NielStevensen@users.noreply.github.com>
2019-08-13 17:08:28 +1000

    fixed accidental revet

--- Assets/Scripts/UI/ArrangeUI.cs
+++ Assets/Scripts/UI/ArrangeUI.cs
@@ -8,12 +8,14 @@ public class ArrangeUI : NetworkBehaviour
 {
 
     public GameObject[] flip;
-    public GameObject[] aboveAvatar;
     public Image[] reverseFill;
     public Material customShader;
+
+    public GameObject[] mobileRefrences; // list of objetcst for rearanging on mkbile
+    Camera cam;
     public void Start()
     {
-#if UNITY_EDITOR || UNITY_STANDALONE
+#if UNITY_STANDALONE
         if (isServer)
         {
             if (!isLocalPlayer)
@@ -55,7 +57,31 @@ public class ArrangeUI : NetworkBehaviour
 
 #elif UNITY_ANDROID // if on an android platform
 
+        cam = FindObjectOfType<Camera>();
+        if (isServer) // set refrences to chain and combo printing
+        {
+            gameObject.GetComponent<MatchUI>().comboDisp = GameObject.FindObjectOfType<UiSetup>().hostInfo[0];
+            gameObject.GetComponent<MatchUI>().chainDisp = GameObject.FindObjectOfType<UiSetup>().hostInfo[1];
+        }
+        else
+        {
+            gameObject.GetComponent<MatchUI>().comboDisp = GameObject.FindObjectOfType<UiSetup>().clientInfo[0];
+            gameObject.GetComponent<MatchUI>().chainDisp = GameObject.FindObjectOfType<UiSetup>().clientInfo[1];
+        } 
+
+        if(isLocalPlayer)
+        {
+            mobileRefrences[0].transform.position = new Vector3(-1.45f, 3.11f, 0);
+            mobileRefrences[1].transform.position = new Vector3(0f, -4.15f, 0);
+            mobileRefrences[1].transform.localScale *= 1.5f;
+            mobileRefrences[2].transform.position = new Vector3(0f, 5.25f, 0);
+        } 
+        else
+        {
+            mobileRefrences[0].transform.position = new Vector3(1.45f, 3.11f, 0);
+        }
+
 #endif
-        
+
     }
 }
