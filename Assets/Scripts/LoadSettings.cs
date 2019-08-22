8c763af6ee7b039d36cf7f3ea6264ab25c8b7ec3
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2019-08-23 06:37:23 +1000

    Changed the name of a script

---
+++ Assets/Scripts/LoadSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadSettings : MonoBehaviour
+{
+    public GameManager Gamemanager = new GameManager();
+    public GreyScale Greyscale;
+    private AudioSource SFX;
+    private AudioSource Music;
+
+    void Start()
+    {
+        if (GameObject.Find("SFXSource") != null)
+        {
+            SFX = GameObject.Find("SFXSource").GetComponent<AudioSource>();
+        }
+
+        if (GameObject.Find("MusicSource") != null)
+        {
+            Music = GameObject.Find("MusicSource").GetComponent<AudioSource>();
+        }
+
+        Load();
+    }
+
+    private void Load()
+    {
+        Gamemanager = JsonUtility.FromJson<GameManager>(File.ReadAllText(Application.persistentDataPath + "/GameSettings.json"));
+
+        SFX.volume = Gamemanager.SFXVolume;
+        Music.volume = Gamemanager.MusicVolume;
+        Greyscale.Saturation = Gamemanager.Saturation;
+        Greyscale.Contrast = Gamemanager.Contrast;
+        Greyscale.Exposure = Gamemanager.Exposure;
+    }
+}
