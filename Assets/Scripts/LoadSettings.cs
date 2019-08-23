192ea5c45090789c57a1a4d81f0b9c83acb50390
a-clements <antony.clements@bigpond.com>
2019-08-23 11:05:41 +1000

    found a bug

--- Assets/Scripts/LoadSettings.cs
+++ Assets/Scripts/LoadSettings.cs
@@ -7,30 +7,23 @@ public class LoadSettings : MonoBehaviour
 {
     public GameManager Gamemanager = new GameManager();
     public GreyScale Greyscale;
-    private AudioSource SFX;
-    private AudioSource Music;
+    public AudioSource SFX;
+    public AudioSource Music;
 
     void Start()
     {
-        if (GameObject.Find("SFXSource") != null)
-        {
-            SFX = GameObject.Find("SFXSource").GetComponent<AudioSource>();
-        }
-
-        if (GameObject.Find("MusicSource") != null)
-        {
-            Music = GameObject.Find("MusicSource").GetComponent<AudioSource>();
-        }
-
-        Load();
-    }
-
-    private void Load()
-    {
         Gamemanager = JsonUtility.FromJson<GameManager>(File.ReadAllText(Application.persistentDataPath + "/GameSettings.json"));
 
-        SFX.volume = Gamemanager.SFXVolume;
-        Music.volume = Gamemanager.MusicVolume;
+        if (SFX != null)
+        {
+            SFX.volume = Gamemanager.SFXVolume;
+        }
+
+        if (Music != null)
+        {
+            Music.volume = Gamemanager.MusicVolume;
+        }
+
         Greyscale.Saturation = Gamemanager.Saturation;
         Greyscale.Contrast = Gamemanager.Contrast;
         Greyscale.Exposure = Gamemanager.Exposure;
